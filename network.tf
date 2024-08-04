0283b796d439b173c9d4adaef65085159064c924
Mayursinh Sarvaiya <my343777@dal.ca>
2024-08-04 17:18:03 -0300

    chore: naming

--- network.tf
+++ network.tf
@@ -46,12 +46,20 @@ resource "aws_subnet" "tidybase_lb_1" {
   vpc_id            = aws_vpc.tidybase_network.id
   availability_zone = "us-east-1a"
   cidr_block        = "10.0.2.0/24"
+
+  tags = {
+    Name = "tidybase-lb-1"
+  }
 }
 
 resource "aws_subnet" "tidybase_lb_2" {
   vpc_id            = aws_vpc.tidybase_network.id
   availability_zone = "us-east-1b"
   cidr_block        = "10.0.3.0/24"
+
+  tags = {
+    Name = "tidybase-lb-2"
+  }
 }
 
 resource "aws_security_group" "tidybase_lb" {
